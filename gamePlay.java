93660bb1dc913ffbd49d5865e1e247debfe44ab3
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-07-11 21:45:45 +0530

    init variables used in the game play

--- gamePlay.java
+++ gamePlay.java
@@ -1,7 +1,16 @@
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
 /**
  * gamePlay
  */
-public class gamePlay {
+public class gamePlay extends JPanel implements KeyListener, ActionListener {
+     private boolean play =false;
+     private int score=0;
+
+     private int totalBricks=21;
 
     
 }
