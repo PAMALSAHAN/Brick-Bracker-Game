32089555dbef6e9aff6c28fd75c8c93ad6f3eb78
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-07-14 22:28:22 +0530

    init acton preformed function

--- gamePlay.java
+++ gamePlay.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.util.Timer;
 
 import javax.swing.JPanel;
+
+import com.sun.glass.events.KeyEvent;
 
 import javafx.scene.layout.BorderStroke;
 
@@ -52,6 +55,34 @@ public class gamePlay extends JPanel implements KeyListener, ActionListener {
         //set ball
         g.setColor(Color.yellow);
         g.fillRect(ballposX, ballposY, 20, 20);
+
+        public void keyTyped(KeyEvent e){ }
+        public void KeyReleased(KeyEvent e){ }
+
+        public void actionPerformed(ActionEvent e){
+            if(e.getKeyCode()==KeyEvent.VK_RIGHT){
+                if(playerX >=600){
+                    playerX=600;
+                }
+                else{
+                    moveRight();
+                }
+            }
+            if(e.getKeyCode()==KeyEvent.VK_LEFT){
+                if(playerX >=10){
+                    playerX=10;
+                }
+                else{
+                    moveLeft();
+                }
+            }
+            //this is to do 
+        }
+       
+        public void KeyPressed(KeyEvent e){
+
+        }
+       
     }
 
 
